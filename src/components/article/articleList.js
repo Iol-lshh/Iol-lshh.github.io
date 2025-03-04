bc6a4f8705d54103303fffb629b05377dc3bafb7
lshh <markhong93@naver.com>
2025-03-04 15:24:43 +0900

    기사 목록 컴포넌트 개선: 스타일 수정 및 최소 높이 설정 추가

--- src/components/article/articleList.js
+++ src/components/article/articleList.js
@@ -13,40 +13,55 @@ const ArticleList = ({ posts }) => {
 
   return (
     <>
-      <ol style={{ listStyle: `none` }}>
-        {currentPosts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          const postCategory = node.frontmatter.category || []
+      <div style={{
+        minHeight: '100vh',
+      }}>
+        <ol style={{ 
+          listStyle: `none`,
+          margin: 0, 
+          padding: 0,
+          width: '100%',
+          boxSizing: 'border-box'
+        }}>
+          {currentPosts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            const postCategory = node.frontmatter.category || []
 
-          return (
-            <li key={node.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <small>{postCategory.join(', ')}</small>
-                  <h2>
-                    <Link to={node.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{node.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+            return (
+              <li key={node.fields.slug} style={{ 
+                height: 'auto', 
+                minHeight: '110px',
+                marginBottom: '1.5rem',
+                position: 'relative'
+              }}>
+                <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <small>{postCategory.join(', ')}</small>
+                    <h2>
+                      <Link to={node.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{node.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: node.frontmatter.description || node.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                </article>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
       <ArticlePagination
         posts={posts}
         postsPerPage={postsPerPage}
